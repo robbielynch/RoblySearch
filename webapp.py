a06bd86be2919cc73d1f457e6187ec4d1be3477e
Robbie Lynch <robbie.lynch@outlook.com>
2014-02-11 00:07:23 +0000

    added ability for user to crawl websites using threads.

--- webapp.py
+++ webapp.py
@@ -1,6 +1,7 @@
 from functools import wraps
 import logging
 import random
+import threading
 
 from flask import Flask, request, session, url_for, redirect, render_template
 from robly_mongo.user_mongo import UserMongo
@@ -8,6 +9,7 @@ from robly_crawler.crawler import get_website_object
 from robly_parser.parser import QueryParser
 from robly_mongo.website_mongo import WebsiteMongo
 from robly_data.random_search_placeholder import search_placeholders
+from robly_crawler import crawler
 
 
 
@@ -61,7 +63,7 @@ def signup():
     """
     Displays the signup form for potential new users.
     """
-    return render_template('index.html', rand_search=random.choice(search_placeholders))
+    return render_template('index.html', rand_search=random.choice(search_placeholders), logged_in=is_logged_in())
 
 
 @webapp.route('/login', methods=["GET", "POST"])
@@ -121,28 +123,70 @@ def search():
     DEBUG_INFO = "[ROBLY] webapp.py - /search - "
     #logging.debug(DEBUG_INFO + "POST request = " + request.form)
     query = request.form['search_box']
-    logging.warning(DEBUG_INFO + "query = " + query)
+    query.strip()
+    logging.debug(DEBUG_INFO + "query = " + query)
+
     #Create query parser object with query string provided by user
     query_parser = QueryParser(query)
     search_query, search_context = query_parser.extract_context_and_search_query()
-    logging.debug(DEBUG_INFO + "SearchQuery = " + search_query)
-    logging.debug(DEBUG_INFO + "SearchContext = " + search_context)
-    #query should now have been pruned of unnecessary words and characters
-    #Get info from database
-    mongo = WebsiteMongo()
-    websites, stats = mongo.search_websites(search_query, search_context)
-    #convert microseconds to seconds
-    seconds = stats.time_micros / 1000000
-    return render_template('search_results.html', search_results=websites, stats=stats, seconds=seconds)
+    if len(query) > 1:
+        logging.debug(DEBUG_INFO + "SearchQuery = " + search_query)
+        logging.debug(DEBUG_INFO + "SearchContext = " + search_context)
+        #query should now have been pruned of unnecessary words and characters
+        #Get info from database
+        try:
+            mongo = WebsiteMongo()
+            logging.debug(DEBUG_INFO + "Attempthing to connect with mongodb searchquery='{}' "
+                                       "context=''".format(search_query, search_context))
+            websites, stats = mongo.search_websites(search_query, search_context)
+            #convert microseconds to seconds
+            seconds = stats.time_micros / 1000000
+            return render_template('search_results.html', search_results=websites, stats=stats, seconds=seconds)
+        except:
+            logging.error(DEBUG_INFO + "Error searching mongodb with the searchquery '{}'".format(search_query))
+            return redirect(url_for('index'))
+    else:
+        return redirect(url_for('index'))
 
 @webapp.route('/index_website', methods=["POST"])
 def index_website():
-    logging.warning("in index_website")
-    logging.warning(request.form)
-    query = request.form['search_box']
-    logging.warning("query = " + query)
-    #return render_template('search_results.html', search_term=query)
-    return query
+    DEBUG_INFO = "[ROBLY] webapp.py - /index_website - "
+    url = request.form['search_box']
+    url.strip()
+    logging.debug(DEBUG_INFO + "url = " + url)
+    #Check if valid url
+    if is_valid_url(url) and url.startswith('http'):
+        logging.debug(DEBUG_INFO + "'{}' is a valid url")
+        #Crawl website in a Thread
+        t1 = threading.Thread(target=crawler.crawl_website_insert_to_database, args=(url,))
+        t1.start()
+        #Return the user to the index page while the thread is executing.
+        return render_template('/index.html', logged_in=is_logged_in(),
+                                   rand_search=random.choice(search_placeholders),
+                                   index_message="'{} is being indexed and will be available to search in a few minutes.'".format(url))
+    else:
+        logging.error(DEBUG_INFO + "'{}' is not a valid url")
+        return render_template('/index.html', logged_in=is_logged_in(),
+                                   rand_search=random.choice(search_placeholders),
+                                   index_message="Could not index the URL provided - {}".format(url))
+
+@webapp.route('/error_indexing', methods=['GET'])
+def error_indexing():
+    return render_template('/index.html', logged_in=is_logged_in(),
+                                   rand_search=random.choice(search_placeholders),
+                                   index_message="Could not index the URL provided")
+
+
+def is_valid_url(url):
+    import re
+    regex = re.compile(
+        r'^https?://'  # http:// or https://
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+[A-Z]{2,6}\.?|'  # domain...
+        r'localhost|'  # localhost...
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+        r'(?::\d+)?'  # optional port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+    return url is not None and regex.search(url)
 
 
 def is_empty(any_structure):
@@ -159,6 +203,12 @@ def is_empty(any_structure):
         return False
     else:
         return True
+
+def is_logged_in():
+    if 'logged-in' in session:
+        return True
+    else:
+        return False
 
 
 @webapp.route('/crawlall')
